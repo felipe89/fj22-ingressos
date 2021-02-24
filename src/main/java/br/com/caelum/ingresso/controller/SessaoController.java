6c9d0266c97bbc76a79c10c0ead667415d5f0aac
Gilmar Felipe <gfelipe.trindade@gmail.com>
2021-02-24 19:10:27 -0300

    ajustandotag controller classe sessaoController

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 
+@Controller
 public class SessaoController {
 	
 	@Autowired
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -47,5 +47,6 @@ public class Sessao {
 		this.filme = filme;
 	} 
 	
+	
 
 }

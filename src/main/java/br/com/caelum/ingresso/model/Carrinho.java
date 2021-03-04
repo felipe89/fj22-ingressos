a7d29e8063c7bb811aeb826a556e63457372cda8
Gilmar Felipe <gfelipe.trindade@gmail.com>
2021-03-03 22:24:24 -0300

    Implementação de logica para compras

---
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -0,0 +1,32 @@
+package br.com.caelum.ingresso.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+
+@Component
+@SessionScope
+public class Carrinho {
+	
+	private List<Ingresso> ingressos = new ArrayList<>();	
+	
+	
+	public void add(Ingresso ingresso) {
+		ingressos.add(ingresso); 	
+	}
+	
+	public void add (List<Ingresso>ingressos) {
+		this.ingressos.addAll(ingressos); 
+	}
+	public void remove(Ingresso ingresso) {
+		this.ingressos.remove(ingresso); 
+	}
+	
+	public boolean isSelecionado(Lugar lugar) {
+		return ingressos.stream().map(Ingresso::getLugar).anyMatch(lugarDoIngresso -> lugarDoIngresso.equals(lugar)); 
+	}
+
+}

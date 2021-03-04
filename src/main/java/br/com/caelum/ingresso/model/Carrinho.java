dc177c92a869e0ca2af87a3257a758e165abdeeb
Gilmar Felipe <gfelipe.trindade@gmail.com>
2021-03-04 20:58:12 -0300

    Criando Fluxo de compra com cartao

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,19 +15,37 @@ public class Carrinho {
 	private List<Ingresso> ingressos = new ArrayList<>();	
 	
 	
+	public List<Ingresso> getIngressos() {
+		return ingressos;
+	}
+	
+	public Compra toCompra() {
+		return new Compra(ingressos); 
+	}
+
+	public void setIngressos(List<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
+
 	public void add(Ingresso ingresso) {
 		ingressos.add(ingresso); 	
 	}
 	
-	public void add (List<Ingresso>ingressos) {
+	public void add (List<Ingresso>ingressos) { 
 		this.ingressos.addAll(ingressos); 
 	}
 	public void remove(Ingresso ingresso) {
 		this.ingressos.remove(ingresso); 
 	}
+	public void limpa() {
+		this.ingressos.clear();
+	}
 	
 	public boolean isSelecionado(Lugar lugar) {
 		return ingressos.stream().map(Ingresso::getLugar).anyMatch(lugarDoIngresso -> lugarDoIngresso.equals(lugar)); 
 	}
-
+	
+	public BigDecimal getTotal() {
+		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO); 
+	}
 }
